6071a68dfb638a0fe66ade0025d9d52cbccc6daf
Kunji Lal Meena <kunjilal976@gmail.com>
2024-07-30 18:38:22 +0530

    created toto list mini project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import ReactDOM from 'react-dom';
+import './components/style.css';
+import Todo from './components/Todo';
+
+  
+
+function App() {
+  return (
+    <Todo/>
+  )
+}
+
+export default App
